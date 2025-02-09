7145e92228f5a32fbd4246c999a22872ac6675dc
Joshua Niam <joshuaniamjy@gmail.com>
2025-02-09 18:31:46 +0800

    Added new remove_item function

--- inventory.py
+++ inventory.py
@@ -5,13 +5,27 @@ class Inventory:
 
     def add_item(self, item: str):
         if item in self.items:
-            self.items[item] += 1
+            self.items[item].quantity += 1
         else:
             self.items.update({item: Item(item, 1)})
         print("Item(s) added successfully.")
 
+    def remove_item(self, item: str, quantity: int):
+        if item in self.items:
+            self.items[item].quantity -= quantity
+            if self.items[item].quantity <= 0:
+                del self.items[item]
+            print("Item(s) removed successfully.")
+        else:
+            print("Item not found")
+
+    def report_items(self):
+        pass
+
 
 inv = Inventory()
 inv.add_item("Dry-fit shirts")
+inv.add_item("Dry-fit shirts")
+inv.remove_item("Dry-fit shirts", 1)
 
 
